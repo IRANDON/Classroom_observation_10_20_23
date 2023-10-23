fbbb065ee32f57c91b2d5daf020f732c27e5300e
Irandon Laureta <86544803+IRANDON@users.noreply.github.com>
2023-10-23 10:49:06 +0800

    Update process_form.php

--- Classroom observation/first page/process_form.php
+++ Classroom observation/first page/process_form.php
@@ -15,9 +15,19 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['create'])) {
         $rating3 = isset($_POST['rating3']) ? intval($_POST['rating3']) : 0;
         $rating4 = isset($_POST['rating4']) ? intval($_POST['rating4']) : 0;
         $rating5 = isset($_POST['rating5']) ? intval($_POST['rating5']) : 0;
+        $rating6 = isset($_POST['rating6']) ? intval($_POST['rating6']) : 0;
+        $rating7 = isset($_POST['rating7']) ? intval($_POST['rating7']) : 0;
+        $rating8 = isset($_POST['rating8']) ? intval($_POST['rating8']) : 0;
+        $rating9 = isset($_POST['rating9']) ? intval($_POST['rating9']) : 0;
+        $rating10 = isset($_POST['rating10']) ? intval($_POST['rating10']) : 0;
+        $rating11 = isset($_POST['rating11']) ? intval($_POST['rating11']) : 0;
+        $rating12 = isset($_POST['rating12']) ? intval($_POST['rating12']) : 0;
+        $rating13 = isset($_POST['rating13']) ? intval($_POST['rating13']) : 0;
+        $rating14 = isset($_POST['rating14']) ? intval($_POST['rating14']) : 0;
 
-        $stmt = $pdo->prepare("INSERT INTO evaluation_data (Uname, Atime, Usubject, Agrade, Adate, Uobserver, rating1, rating2, rating3, rating4, rating5) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-        if ($stmt->execute([$Uname, $Atime, $Usubject, $Agrade, $Adate, $Uobserver, $rating1, $rating2, $rating3, $rating4, $rating5])) {
+        $stmt = $pdo->prepare("INSERT INTO evaluation_data (Uname, Atime, Usubject, Agrade, Adate, Uobserver, rating1, rating2, rating3, rating4, rating5, rating6, rating7, rating8, rating9, rating10, rating11, rating12, rating13, rating14) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+
+        if ($stmt->execute([$Uname, $Atime, $Usubject, $Agrade, $Adate, $Uobserver, $rating1, $rating2, $rating3, $rating4, $rating5, $rating6, $rating7, $rating8, $rating9, $rating10, $rating11, $rating12, $rating13, $rating14])) {
             // Data saved successfully
             echo "Data saved to the database.";
         } else {
