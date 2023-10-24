0bc64d8078d84acf4435406e3dc395160cdda7be
Irandon Laureta <86544803+IRANDON@users.noreply.github.com>
2023-10-24 18:27:01 +0800

    Update process_form.php

--- Classroom observation/first page/process_form.php
+++ Classroom observation/first page/process_form.php
@@ -24,10 +24,11 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['create'])) {
         $rating12 = isset($_POST['rating12']) ? intval($_POST['rating12']) : 0;
         $rating13 = isset($_POST['rating13']) ? intval($_POST['rating13']) : 0;
         $rating14 = isset($_POST['rating14']) ? intval($_POST['rating14']) : 0;
+        $rating15 = isset($_POST['rating15']) ? intval($_POST['rating15']) : 0;
+        
+        $stmt = $pdo->prepare("INSERT INTO evaluation_data (Uname, Atime, Usubject, Agrade, Adate, Uobserver, rating1, rating2, rating3, rating4, rating5, rating6, rating7, rating8, rating9, rating10, rating11, rating12, rating13, rating14, rating15) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 
-        $stmt = $pdo->prepare("INSERT INTO evaluation_data (Uname, Atime, Usubject, Agrade, Adate, Uobserver, rating1, rating2, rating3, rating4, rating5, rating6, rating7, rating8, rating9, rating10, rating11, rating12, rating13, rating14) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-
-        if ($stmt->execute([$Uname, $Atime, $Usubject, $Agrade, $Adate, $Uobserver, $rating1, $rating2, $rating3, $rating4, $rating5, $rating6, $rating7, $rating8, $rating9, $rating10, $rating11, $rating12, $rating13, $rating14])) {
+        if ($stmt->execute([$Uname, $Atime, $Usubject, $Agrade, $Adate, $Uobserver, $rating1, $rating2, $rating3, $rating4, $rating5, $rating6, $rating7, $rating8, $rating9, $rating10, $rating11, $rating12, $rating13, $rating14, $rating15])) {
             // Data saved successfully
             echo "Data saved to the database.";
         } else {
